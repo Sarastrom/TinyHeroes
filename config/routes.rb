ede787b6ac298c8af8981c79a7c41447ec270bad
Sara Strom <saraiestrom@gmail.com>
2019-11-26 16:10:35 +0000

    profile page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :missions, only: [:new, :create, :index, :destroy, :update, :edit]
-  resources :wish_lists, only: [:new, :create, :index, :destroy, :update, :edit]
+  resources :missions, only: [:new, :create, :index, :destroy, :update, :edit] do |variable|
+  get 'profile', to: 'pages#profile'
+  end
+  resources :wish_list, only: [:new, :create, :destroy, :update, :edit]
+
+    get 'profile', to: 'pages#profile'
+    get 'users/:id/wish_list', to: 'wish_list#index', as: "user_wish_list"
 
 end
