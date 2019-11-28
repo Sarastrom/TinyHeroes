2c30bf19bcc7b15c969d8d78bb7c612d447ba078
Ryan Fleming <ryanfleming93@aol.com>
2019-11-28 13:51:38 +0000

    fixed log in and sign up redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,15 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-     protected
+  protected
 
-      def configure_permitted_parameters
-           devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name, :avatar)}
+    def after_sign_in_path_for(resource)
+      missions_path
+    end
 
-           devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :first_name, :last_name, :avatar)}
-      end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :first_name, :last_name, :avatar)}
+
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :first_name, :last_name, :avatar)}
+    end
 end
