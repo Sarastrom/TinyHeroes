2050f1b83523c37424388100dc69011f4210eabb
Ryan Fleming <ryanfleming93@aol.com>
2019-11-29 15:34:44 +0000

    fixed routes, updated controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,15 @@ class UsersController < ApplicationController
     @childrens = current_user.kids
     # @users = User.where(creator: current_user)#.or(Mission.where(user: current_user))
   end
+
+  def create_child
+    @user = User.new(create_child_params)
+    @user.parent = current_user
+    @user.save
+    redirect_to users_family_path
+  end
+
+  def create_child_params
+    params.require(:user).permit(:first_name, :email, :password)
+  end
 end
