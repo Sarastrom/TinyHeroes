34df935cabae799830532ced768d44e9f88be28c
Sara Strom <saraiestrom@gmail.com>
2019-11-29 12:10:15 +0000

    images added and css

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,4 +1,7 @@
 class Mission < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :creator, class_name: :User, foreign_key: :creator_id
+
+  MISSION_ICON = ["book", "washing", "dishes", "dogWalk"]
+
 end
