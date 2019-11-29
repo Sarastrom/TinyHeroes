3ca9cc32f82f22ba7038f14fd78ed3a9056bdd3a
Sara Strom <saraiestrom@gmail.com>
2019-11-29 14:46:59 +0000

    like function

--- app/models/wish_list.rb
+++ app/models/wish_list.rb
@@ -2,4 +2,16 @@ class WishList < ApplicationRecord
   belongs_to :user
   scope :starts_with, ->(name) { where("name like ?", "#{name}%") }
   has_one_attached :photo
+  has_many :likes
+
+  def children_who_liked
+    list = likes.map do |like|
+      like.user.first_name
+    end
+    if list.empty?
+      return "No likes yet 😢"
+    else
+      return "Like by #{list.uniq.to_sentence}"
+    end
+  end
 end
