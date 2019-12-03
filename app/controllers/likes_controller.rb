910e875bb9905aa9cecdb0fe9d157dc0dad6294a
Sara Strom <saraiestrom@gmail.com>
2019-12-03 14:15:56 +0000

    pushing to master2

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -8,7 +8,7 @@ class LikesController < ApplicationController
     @wish_list = WishList.find(params[:like][:wish_list_id])
     @like.wish_list = @wish_list
     @like.user = current_user
-    current_user.reward_amount = current_user.reward_amount - @wish_list.amount.to_i
+    current_user.reward_amount = current_user.reward_amount.to_i - @wish_list.amount.to_i
     current_user.save
     @like.save
     redirect_to request.referrer
